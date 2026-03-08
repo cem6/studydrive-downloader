fcf8098309b0a44897f1b894e671ea7fa080be5f
cem2 <cembahakilit@gmail.com>
2026-03-09 00:59:52 +0100

    cleanup

--- src/background.js
+++ src/background.js
@@ -2,25 +2,7 @@
 
 const allowedExtensions = ["pdf", "docx", "jpeg", "jpg", "png"];
 
-const getDownloadUrl = (html, url) => {
-    const tokenMatch = html.match(/token=(.*?)"/);
-    if (!tokenMatch) return "/not_found: token";
-    const token = tokenMatch[1];
-
-    const urlObj = new URL(url);
-    const docId = urlObj.pathname.split("/").pop();
-
-    const filenameMatch = html.match(/"filename":"(.*?)"/);
-    if (!filenameMatch) return "/not_found: filename";
-    const filename = filenameMatch[1].toLowerCase();
-
-    let extension = allowedExtensions.find(ext => filename.includes(ext));
-    if (!extension) return "/not_found: extension";
-
-    return `https://cdn.studydrive.net/d/prod/documents/${docId}/original/${docId}.${extension}?token=${token}`;
-}
-
-const t_getDownloadUrl = async (url) => {
+const getDownloadUrl = async (url) => {
 	const response = await fetch(url, {
 		method: 'GET',
 		credentials: 'omit', // cookies entfernen -> nicht logged in
@@ -59,22 +41,13 @@ chrome.action.onClicked.addListener(async (tab) => {
         return;
     }
 
-    // // browser.tabs.sendMessage(tab.id, {action: "fetch"})
-    // chrome.tabs.sendMessage(tab.id, {action: "fetch"})
-    //     .then((response) => {
-    //         // console.log(response.html);
-    //         const downloadUrl = getDownloadUrl(response.html, response.url);
-    //         if (downloadUrl.startsWith("/not_found")) {
-    //             console.error(downloadUrl);
-    //             return;
-    //         }
-    //         // browser.tabs.create({ url: downloadUrl });
-    //         chrome.tabs.create({ url: downloadUrl });
-    //     })
-
-	const downloadUrl = await t_getDownloadUrl(tab.url);
+	const downloadUrl = await getDownloadUrl(tab.url);
 	if (!downloadUrl || downloadUrl.startsWith("/not_found")) {
 		console.error(downloadUrl)
+		// chrome.runtime.sendMessage({
+        //     action: "alert", 
+        //     message: "current url: " + tab.url + "\n\n" + "is not a studydrive document page"
+        // })
 		return
 	}
 	chrome.tabs.create({ url: downloadUrl })
